d4e9696ad579dc14874ec8cd26081557f6806177
Damon Rees <90896022+TheAgonyTeam@users.noreply.github.com>
2021-11-10 14:31:07 +0000

    added all javascript to script.js.

--- script.js
+++ script.js
@@ -1,1 +1,19 @@
-// is Script.js needed? - no not really but its here so i might as well put something here lol...
+// to toggle the menu on small screens when clicking on the menu button (index.html (Home))
+function myFunction() {
+  var x = document.getElementById("navDemo");
+  if (x.className.indexOf("w3-show") == -1) {
+    x.className += " w3-show";
+  } else { 
+    x.className = x.className.replace(" w3-show", "");
+  }
+}
+//=======================================================
+// Open and close sidebar (portfolio.html)
+function w3_open() {
+  document.getElementById("mySidebar").style.width = "100%";
+  document.getElementById("mySidebar").style.display = "block";
+}
+
+function w3_close() {
+  document.getElementById("mySidebar").style.display = "none";
+}
